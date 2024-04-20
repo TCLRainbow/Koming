562bb16f8244a3369ac0c893b9f84d47d47a11cf
Pythonic-Rainbow <wongchingho0@gmail.com>
2024-04-20 02:24:44 +0100

    Add render function

--- koming/battlefield.py
+++ koming/battlefield.py
@@ -50,36 +50,48 @@ class Village:
 
 class UIVillage(Village):
 
-    def __init__(self, side_len: int, db: Database, res_path: str):
+    def __init__(self, side_len: int, db: Database, res_path: str, ui_debug: bool):
         super().__init__(side_len, db)
         pygame.display.init()
         pygame.display.set_caption("Pygame Tiled Demo")
         display_info = pygame.display.Info()
         display_w = round(min(display_info.current_w, display_info.current_h) * 0.9)
         print('Window size', display_w)
+
+        self.__size = display_w, display_w
         self.__resource_path = res_path + '/'
-        self.size = display_w, display_w
+        self.__ui_debug = ui_debug
+        self.__bg = self._get_bg_()
         self.running = True
-        self.screen = pygame.display.set_mode(self.size)
-        # self.draw_bg(self.__resource_path + "background.png")
-        self.draw_debug_bg()
+        self.screen = pygame.display.set_mode(self.__size)
 
     def _get_coc_obj_resource_path_(self, obj: _CocObject):
         return f'{self.__resource_path}{obj.RESOURCE_FOLDER_PATH}/{obj.NAME}.png'
 
-    def add_troop(self, cls: Type[_Troop], lvl, scaled_pos):
-        troop = super().add_troop(cls, lvl, scaled_pos)
-        tile_img = pygame.image.load(self._get_coc_obj_resource_path_(troop))
-        self.screen.blit(tile_img, self.village_rect_to_ui(troop.hit_box))
-        return troop
-
-    def add_defence(self, cls: Type[_Defence], lvl, scaled_pos):
-        defence = super().add_defence(cls, lvl, scaled_pos)
-        self.draw_defence(defence, True)
-        return defence
+    def _get_bg_(self):
+        surface = pygame.Surface(self.__size)
+        if self.__ui_debug:
+            cmyk = (
+                (0, 255, 255),
+                (255, 0, 255),
+                (255, 255, 0),
+                (0, 0, 0)
+            )
+            for x in range(self.side_len):
+                color_i = x % 4
+                for y in range(self.side_len):
+                    rect = pygame.Rect((x, y), (1, 1))
+                    self.draw_rect(rect, cmyk[color_i], surface)
+                    color_i = (color_i + 1) % 4
+        else:
+            image = pygame.image.load(self.__resource_path + "background.png")
+            image = pygame.transform.scale(image, self.__size)
+            rect = image.get_rect()
+            surface.blit(image, rect)
+        return surface
 
     def scaled_to_ui_coord(self, scaled: tuple[float, float], translate=0):
-        return self.size[0] * scaled[0] + translate, self.size[1] * scaled[1] + translate
+        return self.__size[0] * scaled[0] + translate, self.__size[1] * scaled[1] + translate
 
     def run(self):
         while self.running:
@@ -93,29 +105,8 @@ class UIVillage(Village):
 
         pygame.quit()
 
-    def draw_bg(self, file):
-        image = pygame.image.load(file)
-        image = pygame.transform.scale(image, self.size)
-        rect = image.get_rect()
-
-        self.screen.blit(image, rect)
-
-    def draw_debug_bg(self):
-        cmyk = (
-            (0, 255, 255),
-            (255, 0, 255),
-            (255, 255, 0),
-            (0, 0, 0)
-        )
-        for x in range(self.side_len):
-            color_i = x % 4
-            for y in range(self.side_len):
-                rect = pygame.Rect((x, y), (1, 1))
-                self.draw_rect(rect, cmyk[color_i])
-                color_i = (color_i + 1) % 4
-
     def village_rect_to_ui(self, rect: pygame.Rect):
-        ratio = self.size[0] / self.side_len
+        ratio = self.__size[0] / self.side_len
         ui_rect = rect.copy()
         ui_rect.update(
             rect.topleft[0] * ratio, rect.topleft[1] * ratio,
@@ -123,12 +114,15 @@ class UIVillage(Village):
         )
         return ui_rect
 
-    def draw_rect(self, rect: pygame.Rect, rgb=(255, 255, 255), scale=True):
-        if scale:
-            ui_rect = self.village_rect_to_ui(rect)
-        else:
-            ui_rect = rect
-        pygame.draw.rect(self.screen, rgb, ui_rect)
+    def draw_rect(self, rect: pygame.Rect, rgb=(255, 255, 255), surface: pygame.Surface = None, scale=True):
+        ui_rect = self.village_rect_to_ui(rect) if scale else rect
+        if surface is None:
+            surface = self.screen
+        pygame.draw.rect(surface, rgb, ui_rect)
+
+    def draw_troop(self, troop: _Troop):
+        tile_img = pygame.image.load(self._get_coc_obj_resource_path_(troop))
+        self.screen.blit(tile_img, self.village_rect_to_ui(troop.hit_box))
 
     def draw_defence(self, defence: _Defence, bound_box=False):
         if bound_box:
@@ -136,3 +130,11 @@ class UIVillage(Village):
             self.draw_rect(bound_rect)
         tile_img = pygame.image.load(self._get_coc_obj_resource_path_(defence))
         self.screen.blit(tile_img, self.village_rect_to_ui(defence.hit_box))
+
+    def render(self):
+        self.screen.blit(self.__bg, (0, 0))
+        for defence in self.defences:
+            self.draw_defence(defence, True)
+        for troop in self.troops:
+            self.draw_troop(troop)
+        pygame.display.update()
--- main.py
+++ main.py
@@ -4,11 +4,11 @@ from koming.objects import *
 
 if __name__ == '__main__':
     db = Database('koming.sqlite')
-    game = UIVillage(44, db, 'resources')
+    game = UIVillage(44, db, 'resources', True)
 
     barb = game.add_troop(Barbarian, 1, (0.2, 0.4))
     cannon = game.add_defence(Cannon, 1, (0.4, 0.6))
     wall = game.add_defence(Wall, 1, (0.1, 0.56))
 
-    pygame.display.update()
+    game.render()
     game.run()
